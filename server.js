2491cc20b04d6197a56906abad9345884d36c684
Daniel Atkinson <dan@nopro.be>
2014-05-20 19:29:32 +0100

    updated package.json to include connect dependancy and serving html via connect

--- server.js
+++ server.js
@@ -1,18 +1,20 @@
 var io = require('socket.io').listen(1337);
 var SerialPort = require("serialport").SerialPort;
 
+var connect = require('connect');
+connect().use(connect.static(__dirname)).listen(8080);
 //Connect to serialport (where the arduino is)
-var serialPort = new SerialPort("/dev/ttyACM0", {
-  baudrate: 9600
-});
+//var serialPort = new SerialPort("/dev/ttyACM0", {
+//  baudrate: 9600
+//});
 
 //Only register the listeners when it's okay to do so.
 //serialPort.on("open", function () {
 	//Web sockets lolwot
 	io.sockets.on('connection', function (socket) {
- 		socket.on('cmd', function (data) {
- 			console.log(data);
-			serialPort.write(data);
-  		});
-	});
+    socket.on('cmd', function (data) {
+      console.log(data);
+      serialPort.write(data);
+    });
+  });
 //});
