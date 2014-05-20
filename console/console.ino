8979d89e91ce6e37dc07926239305b3543fa6f86
Daniel Atkinson <daniel.atkinson@drpgroup.com>
2014-05-20 12:56:34 +0100

    button and power changes are worked out and debug print is sent over serial

--- console/console.ino
+++ console/console.ino
@@ -13,6 +13,7 @@ const int fireButtonPin = 22;
 const int ledPin = 13;
 
 // Variables will change:
+int iteration = 0;  //  global variable used for iterating
 int ledState = HIGH;
 int fireButtonState;
 int lastFireButtonState = LOW;
@@ -26,6 +27,7 @@ typedef struct
   int pin;
   int previousState;
   int currentState;
+  int debounce;
 } button_states;
 
 button_states buttons[5];
@@ -36,6 +38,7 @@ typedef struct
   int pin_two;
   int pin_three;
   int power;
+  int previousPower;
 } power_states;
 
 power_states power[3];
@@ -97,55 +100,60 @@ void setup() {
 }
 
 void loop() {
-  // read the state of the switch into a local variable:
-  int reading = digitalRead(fireButtonPin);
+update_buttons();
+update_power();
+}
 
-  // check to see if you just pressed the button 
-  // (i.e. the input went from LOW to HIGH),  and you've waited 
-  // long enough since the last press to ignore any noise:  
+void update_buttons() {
+  for (iteration = 0; iteration < sizeof(buttons) -1; iteration++){
+//    buttons[iteration].previousState = buttons[iteration].currentState;
+    // read the state of the switch into a local variable:
+    buttons[iteration].currentState = digitalRead(buttons[iteration].pin);
 
-  // If the switch changed, due to noise or pressing:
-  if (reading != lastFireButtonState) {
-    // reset the debouncing timer
-    lastDebounceTime = millis();
-  } 
+    // check to see if you just pressed the button 
+    // (i.e. the input went from LOW to HIGH),  and you've waited 
+    // long enough since the last press to ignore any noise:  
+    // If the switch changed, due to noise or pressing:
+    if (buttons[iteration].currentState != buttons[iteration].previousState) {
+      // reset the debouncing timer
+      buttons[iteration].debounce = millis();
+    } 
   
-  if ((millis() - lastDebounceTime) > debounceDelay) {
-    // whatever the reading is at, it's been there for longer
-    // than the debounce delay, so take it as the actual current state:
-
-    // if the button state has changed:
-    if (reading != fireButtonState) {
-      fireButtonState = reading;
-
-      // only toggle the LED if the new button state is HIGH
-      if (fireButtonState == HIGH) {
-        ledState = !ledState;
+    if ((millis() - buttons[iteration].debounce) > debounceDelay) {
+      // whatever the reading is at, it's been there for longer than the debounce delay, so take it as the actual current state:
+      // if the button state has changed:
+      if (buttons[iteration].currentState != buttons[iteration].previousState) {
+        buttons[iteration].previousState = buttons[iteration].currentState;
+        // only toggle the LED if the new button state is HIGH
+        if (buttons[iteration].currentState == HIGH) {
+          Serial.print("Button ");
+          Serial.print(iteration);
+          Serial.println(" has been pressed");
+        }
       }
     }
-  }
-  
-  // set the LED:
-  digitalWrite(ledPin, ledState);
-
-  // save the reading.  Next time through the loop,
-  // it'll be the lastFireButtonState:
-  lastFireButtonState = reading;
+  } 
 }
 
 void update_power() {
-  for (int iteration = 0; iteration < sizeof(power) - 1; iteration++){
-    int temp_power = 0;
+  for (iteration = 0; iteration < sizeof(power) - 1; iteration++){
+    power[iteration].previousPower = power[iteration].power;
+    power[iteration].power = 0;
     if (digitalRead(power[iteration].pin_one) == HIGH){
-      temp_power++;
+      power[iteration].power++;
     }
     if (digitalRead(power[iteration].pin_two) == HIGH){
-      temp_power++;
+      power[iteration].power++;
     }
     if (digitalRead(power[iteration].pin_three) == HIGH){
-      temp_power++;
+      power[iteration].power++;
     }
-    power[iteration].power = temp_power;
+    if (power[iteration].power != power[iteration].previousPower) {
+      Serial.print("System ");
+      Serial.print(iteration);
+      Serial.print(" has a new power rating of ");
+      Serial.println(power[iteration].power);
+    }
   }
 }
 
